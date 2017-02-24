454c482709fc97a9121be0083ad0d4850f30f132
jiabotao2015 <jiabotao@gmail.com>
2017-02-24 08:50:21 +0800

    sadasd
    sss

--- Phantom-API/src/Phantom/API/Test.java
+++ Phantom-API/src/Phantom/API/Test.java
@@ -1,9 +1,16 @@
 package Phantom.API;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		List<String> namelist = new ArrayList<String>();
+		namelist.add("123");
+		namelist.add("234");
+		String[] names = namelist.toArray(new String[namelist.size()]); 
+		System.out.println(names);
 
 	}
 
