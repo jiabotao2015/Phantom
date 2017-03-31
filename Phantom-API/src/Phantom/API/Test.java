41ecd31b7528126b194b897a459eeef118ec7263
jiabotao2015 <jiabotao@gmail.com>
2017-03-31 17:28:00 +0800

    GPS Encript

--- Phantom-API/src/Phantom/API/Test.java
+++ Phantom-API/src/Phantom/API/Test.java
@@ -3,14 +3,19 @@ package Phantom.API;
 import java.util.ArrayList;
 import java.util.List;
 
+import Phantom.API.Bean.LonLat;
+import Phantom.API.Util.LngLatUtil;
+
 public class Test {
 
 	public static void main(String[] args) {
-		List<String> namelist = new ArrayList<String>();
-		namelist.add("123");
-		namelist.add("234");
-		String[] names = namelist.toArray(new String[namelist.size()]); 
-		System.out.println(names);
+		LonLat coord = new LonLat();
+		double wgLat = 30.50103;
+		double	wgLon = 114.42840;
+		LngLatUtil.transform(wgLat, wgLon, coord);
+		System.out.println(coord.getLatitude());
+		System.out.println(coord.getLongitude());
+		
 
 	}
 
