c3534ffe88f1c9d10c931a054d21f85ac7d5e2fc
jiabotao2015 <jiabotao@gmail.com>
2017-04-06 18:22:35 +0800

    s

--- Phantom-Web/WebContent/AppJS/GIS/MainMap.js
+++ Phantom-Web/WebContent/AppJS/GIS/MainMap.js
@@ -1,10 +1,9 @@
 /**
  * 
  */
-var center = ol.proj.transform([ 114.298729,30.574752 ], 'EPSG:4326',
-		'EPSG:3857');
+var center = ol.proj.transform([ 114.433909, 30.498707 ], 'EPSG:4326','EPSG:3857');
 var origin = [ -20037508.3427892, 20037508.3427892 ];
-var extent = [ -20147508.3427892, -20147508.3427892, 20137508.3427892, 20137508.3427892  ];
+var extent = [ -20037508.3427892, -20037508.3427892, 20037508.3427892, 20037508.3427892 ];
 var resolutions = [];
 for (var i = 0; i < 21; i++) {
 	resolutions[i] = (extent[2] - extent[0]) / 256 / (Math.pow(2, i));
@@ -33,7 +32,7 @@ var gaode_source = new ol.source.TileImage(
 				var z = tileCoord[0];
 				var x = tileCoord[1];
 				var y = -tileCoord[2] - 1;
-				//var url = "http://wprd02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}"
+
 				var url = "http://webst02.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=7&x={x}&y={y}&z={z}";
 				url = url.replace("{z}", z);
 				url = url.replace("{x}", x);
@@ -41,10 +40,6 @@ var gaode_source = new ol.source.TileImage(
 				return url;
 			}
 		});
-
-var gaode_layer = new ol.layer.Tile({
-	source : gaode_source
-});
 
 var gaode_layer = new ol.layer.Tile({
 	source : gaode_source
