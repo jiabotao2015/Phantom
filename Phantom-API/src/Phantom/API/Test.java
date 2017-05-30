ee620a458b1ca10ee27a214c44e89afbb6bff448
jiabotao2015 <jiabotao@gmail.com>
2017-05-31 01:48:49 +0800

    SpatialUtil

--- Phantom-API/src/Phantom/API/Test.java
+++ Phantom-API/src/Phantom/API/Test.java
@@ -1,25 +1,10 @@
 package Phantom.API;
 
 
-import com.vividsolutions.jts.geom.Point;
-
-import Phantom.API.Bean.LonLat;
-import Phantom.API.Util.LngLatUtil;
-import Phantom.API.Util.SpatialUtil;
 
 public class Test {
 
 	public static void main(String[] args) {
-		/*LonLat coord = new LonLat();
-		double wgLat = 30.50103;
-		double	wgLon = 114.42840;
-		LngLatUtil.transform(wgLat, wgLon, coord);
-		System.out.println(coord.getLatitude());
-		System.out.println(coord.getLongitude());*/
-		
-		SpatialUtil su = new SpatialUtil();
-		Point pt = su.createPoint();
-		System.out.println(pt.getSRID());
 		
 
 	}
--- Phantom-API/src/Phantom/API/Util/SpatialUtil.java
+++ Phantom-API/src/Phantom/API/Util/SpatialUtil.java
@@ -1,61 +1,108 @@
 package Phantom.API.Util;
 
+import org.osgeo.proj4j.CRSFactory;
+import org.osgeo.proj4j.CoordinateReferenceSystem;
+import org.osgeo.proj4j.CoordinateTransform;
+import org.osgeo.proj4j.CoordinateTransformFactory;
+import org.osgeo.proj4j.ProjCoordinate;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKTReader;
 
 public class SpatialUtil {
 	
-	private GeometryFactory geometryFactory = new GeometryFactory();
-	
-	public Point createPoint(){
-        Coordinate coord = new Coordinate(109.013388, 32.715519);
-        //coord.setOrdinate(ordinateIndex, value);
-        Point point = geometryFactory.createPoint( coord );
-        return point;
-    }
+	private static CRSFactory crsFactory = new CRSFactory();
+	private static CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
+	private static CoordinateReferenceSystem WGS84 = crsFactory.createFromName("EPSG:4326");
+	private static CoordinateReferenceSystem WebMercator = crsFactory.createFromName("EPSG:3857");
+	private static CoordinateTransform WGS84ToWebMercator = ctFactory.createTransform(WGS84, WebMercator);
+	private static GeometryFactory geometryFactory = new GeometryFactory();
+	private static final int EARTH_RADIUS = 6378137;
 	
 	/**
-	 * 返回(A)与(B)中距离最近的两个点的距离
-	 * @param a
-	 * @param b
+	 * 无论什么坐标系都能用此方法创造点
+	 * @param lon
+	 * @param lat
 	 * @return
 	 */
-	public double distanceGeo(Geometry geomA, Geometry geomB) {
-        return geomA.distance(geomB);
-    }
+	public static Point CreatePoint(double lon, double lat) {
+		Coordinate coord = new Coordinate(lon, lat);
+		Point point = geometryFactory.createPoint(coord);
+		return point;
+	}
 	
 	/**
-     * 返回a指定距离内的多边形和多多边形
-     * @param geom
-     * @param distance
-     * @return
-     */
-	public Geometry bufferGeo(Geometry geom, double distance) {
-        return geom.buffer(distance);
-    }
-	
-	
-	/**
-     * 两个几何对象的交集
-     * @param geomA
-     * @param geomB
-     * @return newgeom
-     */
-	public Geometry intersectionGeo(Geometry geomA, Geometry geomB) {
-        return geomA.intersection(geomB);
-    }
+	 * Well-Know-Text 字符串创建geom对象，任何geom对象
+	 * @param wkt
+	 * @return
+	 */
+	public static Geometry createGeometryByWKT(String wkt) {
+		WKTReader reader = new WKTReader();
+		Geometry geom = null;
+		try {
+			geom = reader.read(wkt);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return geom;
+	}
 	
 	/**
-     * 几何对象合并
-     * @param geomA
-     * @param geomB
-     * @return newgeom
-     */
-    public Geometry unionGeo(Geometry geomA, Geometry geomB) {
-        return geomA.union(geomB);
-    }
+	 * 通过经纬度半径创建圆形，实心圆
+	 * @param lat
+	 * @param lon
+	 * @param radius
+	 * @return
+	 */
+	public static Polygon createCircle(double lat, double lon, double radius) {
+		int sides = 36;// 圆上点个数；
+		//GeometryFactory geometryFactory = new GeometryFactory();
+		Coordinate coords[] = new Coordinate[sides + 1];
+		for (int i = 0; i < sides; i++) {
+			double angle = ((double) i / (double) sides) * Math.PI * 2.0;
+			double dx = Math.cos(angle) * radius;
+			double dy = Math.sin(angle) * radius;
+			coords[i] = new Coordinate((double) lat + dx, (double) lon + dy);
+		}
+		coords[sides] = coords[0];//首尾相连的coord 组层一个环形
+		LinearRing ring = geometryFactory.createLinearRing(coords);
+		Polygon circle = geometryFactory.createPolygon(ring, null);//后面的参数代表挖空，我的天，好高大上的参数
+		return circle;
+	}
+	
+	/**
+	 * WGS84坐标系计算两点之间距离
+	 * @param pointA
+	 * @param pointB
+	 * @return
+	 */
+	public static double getDistance(Point pointA, Point pointB) {
+		double lonA = pointA.getX();
+		double lonB = pointB.getX();
+		double radlatA = toRadians(pointA.getY());
+		double radlatB = toRadians(pointB.getY());
+		double dradLat = radlatB - radlatA;
+		double dradLon = toRadians(lonB - lonA); // 经度角度差
 
+		double c = Math.sin(dradLat / 2) * Math.sin(dradLat / 2)
+				+ Math.sin(dradLon / 2) * Math.sin(dradLon / 2) * Math.cos(radlatA) * Math.cos(radlatB);
+		double distance = 2 * EARTH_RADIUS * Math.atan2(Math.sqrt(c), Math.sqrt(1 - c));
+
+		return (double) Math.round(distance * 100) / 100;
+	}
+
+	public  static ProjCoordinate transform(ProjCoordinate coordA, ProjCoordinate coordB) {
+		return WGS84ToWebMercator.transform(coordA, coordB);
+	}
+	
+	private static double toRadians(double angleInDegrees) {
+		return angleInDegrees * Math.PI / 180;
+	}
 
 }
