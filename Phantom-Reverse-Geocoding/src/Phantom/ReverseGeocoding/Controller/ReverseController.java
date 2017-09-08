7fe463a3f68f96c878b87183236efbaedff5de94
jiabotao2015 <jiabotao@gmail.com>
2017-09-08 13:25:49 +0800

    delete hibernate

--- Phantom-Reverse-Geocoding/src/Phantom/ReverseGeocoding/Controller/ReverseController.java
+++ Phantom-Reverse-Geocoding/src/Phantom/ReverseGeocoding/Controller/ReverseController.java
@@ -1,19 +1,11 @@
 package Phantom.ReverseGeocoding.Controller;
 
-import java.sql.SQLException;
-import java.util.List;
-
-import org.postgis.Geometry;
-import org.postgis.LineString;
-import org.postgis.PGgeometry;
-import org.postgis.Point;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import Phantom.ReverseGeocoding.Service.GeoService;
 import Phantom.ReverseGeocoding.Service.ReverseGeocodingService;
 
 @Controller
@@ -22,8 +14,6 @@ public class ReverseController {
 	@Autowired
 	private ReverseGeocodingService service;
 	
-	@Autowired
-	private GeoService geoservice;
 	
 		
 	@RequestMapping(value="/ReverseEncoding",method=RequestMethod.GET,produces="text/html;charset=UTF-8")
@@ -43,29 +33,6 @@ public class ReverseController {
 		System.out.println("Difference is : " + diff);
 		return result;
 	}
-	
-	@RequestMapping(value="/ReverseEncodingWithHibernate",method=RequestMethod.GET,produces="text/html;charset=UTF-8")
-	public @ResponseBody String ReverseEncodingWithHibernate(String lon,String lat) throws Exception{
-		long start = System.currentTimeMillis();
-		String result  = service.reserve(lon, lat);
-		//String result  = service.reserveWithPool(lon, lat);
-		
-		
-		double a = 114;
-		double b = 30;
-		for(int i=0;i<2000;i++){
-			b = b + 0.0001;
-			result  = service.reserve(a+"", b+"");
-		}
-		long end = System.currentTimeMillis();
-		long diff = end - start;
-		System.out.println("Difference is : " + diff);
-		return result;
-	}
-	
-	
-	
-	
 	
 	
 
--- Phantom-Reverse-Geocoding/src/Phantom/ReverseGeocoding/Service/ReverseGeocodingService.java
+++ Phantom-Reverse-Geocoding/src/Phantom/ReverseGeocoding/Service/ReverseGeocodingService.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
-
-import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,27 +13,10 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 @Service
 public class ReverseGeocodingService {
 	
-	@Autowired
-	private BaseJpaSearch jpaSearch;
 	
 	@Autowired
 	private GeoService geoService;
 	
-	
-	/**
-	 * 执行逆地址解析存储过程
-	 * @param lon
-	 * @param lat
-	 * @return
-	 * @throws Exception
-	 */
-	public String reserve(String lon, String lat) throws Exception{
-		String reversesql = "select reverse("+lon+","+lat+")";
-		List<Object[]> reverseObj = jpaSearch.findBySQL(reversesql);
-		Object[] reversearr = reverseObj.toArray();
-		String reverse = reversearr[0].toString();
-		return reverse;
-	}
 	
 	public String reserveWithPool(String lon,String lat) throws SQLException{
 		ComboPooledDataSource dsp = geoService.getDataSourcePool();
