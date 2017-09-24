0feba2a42b6be14526851f543cf00730f2d5cc6b
jiabotao2015 <jiabotao@gmail.com>
2017-09-24 22:10:55 +0800

    clear
    clear

---
+++ Phantom-Reverse-Geocoding/src/Phantom/ReverseGeocoding/Controller/DataSyn/RoadNameSynController.java
@@ -0,0 +1,11 @@
+package Phantom.ReverseGeocoding.Controller.DataSyn;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RoadNameSynController {
+	
+	public void getRoadFromGaode(String gaodekey){
+		
+	}
+}
